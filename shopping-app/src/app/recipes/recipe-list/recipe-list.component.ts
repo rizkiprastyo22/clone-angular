0ea2c81754f7168d865f6f091a9df93304cd30f9
Rizki Prastyo <rizkiprastyo22@gmail.com>
2022-10-26 11:06:41 +0700

    sudah bisa tambah shopping list

--- shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,7 +13,7 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a Simple Test', 'https://www.seriouseats.com/thmb/K40kegtcsC9WbzOCvogu-sjU79k=/1500x1125/filters:fill(auto,1)/butter-basted-pan-seared-steaks-recipe-hero-06-03b1131c58524be2bd6c9851a2fbdbc3.jpg'),
-    new Recipe('Another Recipe', 'This is another recipe', 'https://www.seriouseats.com/thmb/K40kegtcsC9WbzOCvogu-sjU79k=/1500x1125/filters:fill(auto,1)/butter-basted-pan-seared-steaks-recipe-hero-06-03b1131c58524be2bd6c9851a2fbdbc3.jpg')
+    new Recipe('Another Recipe', 'This is another recipe', 'https://www.gourmetcaterers.com/wp-content/uploads/2020/02/021420-351-scaled.jpg?x46180')
   ];
 
   constructor() { }
